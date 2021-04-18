2b2bd8c546ac2348a0ba88b4808348c26564d5da
Jiyeon526 <b8480@naver.com>
2021-04-18 16:07:21 +0900

    문자열

--- 백준/5430_AC.py
+++ 백준/5430_AC.py
@@ -6,38 +6,37 @@ Test_ = int(input())
 
 while Test_ > 0:
     Test_ -= 1
-    total_fun = input()
-    length = int(input())
+    total_fun = input() # 수행해야되는 함수
+    length = int(input()) # 문자열 길이
 
-    if length == 0:
+    if length == 0: # 만약에 빈 문자열이라면
         trash = input()
-        if 'D' in total_fun:
+        if 'D' in total_fun: # D함수 실행시 error
             print('error')
-        else:
+        else: # R은 오류가 아닌 빈 배열 출력
             print('[]')
         continue
 
     lst = list(map(int, input().replace('[','').replace(']','').replace('\n','').split(',')))
     check = False
-    check_rev = 0
+    check_rev = 0 # 뒤집었는지 확인
     for fun in total_fun:
-        # print("lst = ", lst)
-        if len(lst) == 0 and fun == 'D':
+        if len(lst) == 0 and fun == 'D': 
             print('error')
-            check = True
+            check = True # 에러를 출력했는지 체크
             break 
         if fun == 'R':
-            if check_rev == 0:
+            if check_rev == 0: # 뒤집었다면 체크
                 check_rev = 1
             else:
                 check_rev = 0
         elif fun == 'D':
-            if check_rev == 1:
+            if check_rev == 1: # 뒤집은 상태라면 맨 뒤를 제거
                 lst.pop()
-            else:
+            else: # 그게 아니라면 맨 앞을 제거
                 lst.pop(0)
         
-    if not check:
+    if not check: # 에러를 출력하지 않았으면 밑을 출력한다.
         if check_rev == 1:
             lst.reverse()
 
