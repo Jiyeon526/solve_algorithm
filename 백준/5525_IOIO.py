5e14dfdb87e5d9470a6f9aa1cd7947e860271494
Jiyeon526 <b8480@naver.com>
2021-04-18 15:57:06 +0900

    문자열

--- 백준/5525_IOIO.py
+++ 백준/5525_IOIO.py
@@ -5,23 +5,23 @@ input = sys.stdin.readline
 N = int(input())
 M = int(input())
 S = input()
-P = "I" + "OI" * N
+P = "I" + "OI" * N # 찾을 문자열 만들기
 
 plen = len(P)
 idx = 0
 ans = 0
-check = False
+check = False # 이어지는 문자열인지 확인
 
 while idx <= M:
     if check:
-        if S[idx:idx+2] == "OI":
+        if S[idx:idx+2] == "OI": 
             ans += 1
             idx = idx+2
         else:
-            check = False
+            check = False # 만약 아니라면 check를 풀어서 다시 I부터 비교하게 하기
     else:
-        if S[idx] == "I":
-            if S[idx:idx+plen] == P:
+        if S[idx] == "I": # 문자열 맨 앞비교
+            if S[idx:idx+plen] == P: # 문자열을 p의 길이만큼 잘라서 p와 같은지 확인하기
                 check = True
                 ans += 1
                 idx = idx + plen
