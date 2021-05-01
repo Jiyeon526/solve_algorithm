bd94af431d2c439fbe164be3370122a75265492e
Jiyeon526 <b8480@naver.com>
2021-05-01 11:11:45 +0900

    다익스트라

--- 백준/4485_녹색 옷 입은 애가 젤다지.py
+++ 백준/4485_녹색 옷 입은 애가 젤다지.py
@@ -15,32 +15,28 @@ while True:
         board[i] = list(map(int, input().split()))
     
     rupee = [[100000000 for _ in range(N)] for _ in range(N)]
-    rupee[0][0] = board[0][0]
+    rupee[0][0] = board[0][0] # 처음 시작값 저장하기
     
     hq = []
     heapq.heappush(hq, (rupee[0][0], 0, 0))
 
-    visit = [[False for _ in range(N)] for _ in range(N)]
-    # visit[0][0] = True
-
-    ans = 100000000000000
+    ans = 100000000000000 # 답
     dx = [1, -1, 0, 0]
     dy = [0, 0, 1, -1]
+
     while hq:
         R, X, Y = heapq.heappop(hq)
 
         if X == N-1 and Y == N-1:
             ans = min(ans, R)
 
-        for i in range(4):
+        for i in range(4): # 상하좌우 이동
             nx = X + dx[i]
             ny = Y + dy[i]
 
-            # if 0<=nx<=N-1 and 0<=ny<=N-1 and not visit[nx][ny]:
             if 0<=nx<=N-1 and 0<=ny<=N-1:
-                if rupee[nx][ny] > R + board[nx][ny]:
-                    rupee[nx][ny] = R + board[nx][ny]
-                    # visit[nx][ny] = True
+                if rupee[nx][ny] > R + board[nx][ny]: # 만약 루피값이 이때까지 이동한 값 + 해당 위치의 루피값보다 크다면
+                    rupee[nx][ny] = R + board[nx][ny] # 루피값갱신
                     heapq.heappush(hq, (rupee[nx][ny], nx, ny))
 
     S = "Problem " + str(tc) + ": " + str(ans)
